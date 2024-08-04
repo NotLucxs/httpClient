16734fff68ca0f24a7be5ac75414c8c36bf22c84
Luke McMahon <lucasmcmahon99@gmail.com>
2024-08-04 15:25:50 +0100

    Removed heartbeat. Added POST request

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,13 +3,9 @@ package org.example;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.SocketChannel;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    private static final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
     private static final InetSocketAddress server = new InetSocketAddress("localhost", 8080);
 
     public static void main(String[] args) {
@@ -19,12 +15,8 @@ public class Main {
             //  Create client
             SocketChannel client = SocketChannel.open(server);
             WriteDataThread first = new WriteDataThread(client);
-            Heartbeat heartbeat = new Heartbeat(client);
-            //  Schedule heartbeat on separate thread every 5 seconds
-            executor.scheduleAtFixedRate(heartbeat::run, 0, 5, TimeUnit.SECONDS);
 
             first.run();
-            //  Keep commented to maintain heartbeat
             //client.close();
 
         } catch(IOException e){
--- src/main/java/org/example/WriteDataThread.java
+++ src/main/java/org/example/WriteDataThread.java
@@ -5,14 +5,21 @@ import java.nio.channels.SocketChannel;
 
 public class WriteDataThread implements Runnable{
 
-    private static final String GET_REQUEST = "GET /docs/tutorials/linux/shellscripts/howto.html HTTP/1.1\n"+
-                                                "Host: Linode.com\n"+
-                                                "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.8) Gecko/20091102 Firefox/3.5.5\n"+
-                                                "Accept: text/html,application/xhtml+xml,\n"+
-                                                "Accept-Language: en-us\n"+
-                                                "Accept-Encoding: gzip,deflate\n"+
-                                                "Accept-Charset: ISO-8859-1,utf-8\n"+
-                                                "Cache-Control: no-cache";
+    private static final String GET_REQUEST = "GET /docs/tutorials/linux/shellscripts/howto.html HTTP/1.1\r\n"+
+                                                "Host: Linode.com\r\n"+
+                                                "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.8) Gecko/20091102 Firefox/3.5.5\r\n"+
+                                                "Accept: text/html,application/xhtml+xml,\r\n"+
+                                                "Accept-Language: en-us\r\n"+
+                                                "Accept-Encoding: gzip,deflate\r\n"+
+                                                "Accept-Charset: ISO-8859-1,utf-8\r\n"+
+                                                "Cache-Control: no-cache\r\n\r\n";
+
+    private static final String POST_REQUEST = "POST /test HTTP/1.1\r\n" +
+            "Host: foo.example\r\n" +
+            "Content-Type: application/x-www-form-urlencoded\r\n" +
+            "Content-Length: 27\r\n" +
+            "\r\n\r\n" +
+            "field1=value1&field2=value2";
 
     SocketChannel socketChannel;
 
@@ -23,10 +30,10 @@ public class WriteDataThread implements Runnable{
 
     public void run() {
         try{
-            byte [] message = GET_REQUEST.getBytes();
+            byte [] message = POST_REQUEST.getBytes();
             ByteBuffer buffer = ByteBuffer.wrap(message);
             socketChannel.write(buffer);
-            System.out.println(GET_REQUEST);
+            System.out.println(POST_REQUEST);
             buffer.clear();
         } catch(Exception e){
             e.printStackTrace();
